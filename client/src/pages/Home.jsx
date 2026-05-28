9053b8cf7793cbc848f79d203bce364d92ebe774
Saksham Negi <negisaksham95@gmail.com>
2026-05-28 18:03:34 +0530

    Worked on Banner and Hero Section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Home/Banner'
+import Hero from '../components/Home/Hero'
+
+const Home = () => {
+  return (
+    <div>
+        <Banner />
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
