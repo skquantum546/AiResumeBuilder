d5d62f9ce13ac9c7812ba4cf0c0e376eab4cfb32
Saksham Negi <negisaksham95@gmail.com>
2026-05-29 10:18:57 +0530

    added feature banner and title section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Home/Banner'
 import Hero from '../components/Home/Hero'
+import Feature from '../components/Home/Feature'
 
 const Home = () => {
   return (
     <div>
         <Banner />
         <Hero />
+        <Feature />
     </div>
   )
 }
