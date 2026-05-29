b40599798881802c077af1ca0c86ebda46660042
Saksham Negi <negisaksham95@gmail.com>
2026-05-29 10:53:06 +0530

    added footer completed home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Banner from '../components/Home/Banner'
 import Hero from '../components/Home/Hero'
 import Feature from '../components/Home/Feature'
 import Testimonial from '../components/Home/Testimonial'
+import CallToAction from '../components/Home/CallToAction'
+import Footer from '../components/Home/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
         <Hero />
         <Feature />
         <Testimonial />
+        <CallToAction />
+        <Footer />
     </div>
   )
 }
