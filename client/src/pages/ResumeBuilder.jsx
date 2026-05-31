a1eb0ada2bc204214b30f008d0741286efd9ff47
Saksham Negi <negisaksham95@gmail.com>
2026-05-31 14:54:05 +0530

    Added ColorPicker feature in ResumeBuilder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -6,6 +6,7 @@ import { useState,useEffect } from 'react';
 import PersonalInfoForm from '../components/PersonalInfoForm';
 import ResumePreview from '../components/ResumePreview';
 import TemplateSelector from '../components/TemplateSelector';
+import ColorPicker from '../components/ColorPicker';
 const ResumeBuilder = () => {
 
   const {resumeId}=useParams();
@@ -71,6 +72,7 @@ const ResumeBuilder = () => {
                 <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
                     <div className="flex items-center gap-2">
                       <TemplateSelector selectedTemplate={resumeData.template} onChange={(template)=>setResumeData(prev=>({...prev,template}))}/>
+                      <ColorPicker selectedColor={resumeData.accent_color} onChange={(color)=>setResumeData(prev=>({...prev,accent_color:color}))}/>
                     </div>
                     <div className="flex items-center">
                       {activeSectionIndex!==0 && (
