6649a292e8bcb125d957dc272a359a41ccbb9943
Saksham Negi <negisaksham95@gmail.com>
2026-06-01 10:52:36 +0530

    Added ExperienceForm component in ResumeBuilder

--- client/src/components/ProfessionalSummaryForm.jsx
+++ client/src/components/ProfessionalSummaryForm.jsx
@@ -17,7 +17,7 @@ const ProfessionalSummaryForm = ({data,onChange,setResumeData}) => {
 
         <div className="mt-6">
             <textarea value={data || ""} onChange={(e)=>onChange(e.target.value)} rows={7} className="w-full p-3 px-4 mt-2 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none" placeholder="Write a compelling professional summary that highlights your key strengths and carrer options"/>
-            <p>Tip:Keep it concise (3-4 sentences) and focus on your most relevant achievements and skills.</p>
+            <p className="text-xs text-gray-500 max-w-4/5 mx-auto text-center">Tip:Keep it concise (3-4 sentences) and focus on your most relevant achievements and skills.</p>
         </div>
     </div>
   )
