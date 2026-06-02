a5b2884f57959cae22a112663807f6dd8ef59beb
Saksham Negi <negisaksham95@gmail.com>
2026-06-02 15:35:40 +0530

    Setting up Backend

---
+++ server/configs/db.js
@@ -0,0 +1,28 @@
+import momgoose from "mongoose";
+
+
+const connectDB = async ()=>{
+    try{
+       mongoose.connection.on("connected",async ()=>{
+            console.log("Database connected succesfully");
+
+            let mongodbURI=process.env.MONGODB_URI;
+            const projectName="resume-builder";
+
+            if(!mongodbURI){
+                throw new Error("MONGODB_URI environment variable not set");
+            }
+
+            if(mongodbURI.endsWith('/')){
+                mongodbURI=mongodbURI.slice(0,-1);
+            }
+
+            await mongoose.connect(`${mongodbURI}/${projectName}`);
+       })
+    }
+    catch(error){
+        console.error("Error connecting to MongoDB:",error);
+    }
+}
+
+export default connectDB;
